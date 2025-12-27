1ba067c5591045680f71939d5d19f4106331b02e
Yazan Hailat <yazanhailat@Yazans-MacBook-Pro.local>
2025-12-27 17:42:28 -0500

    new job

--- src/landing/Experince.jsx
+++ src/landing/Experince.jsx
@@ -9,7 +9,8 @@ function Experiences() {
             <div className="flex justify-center items-center flex-wrap gap-24 mt-8">
                 <a href="https://www.altura.com/"
                    className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-lg w-80 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
-                    <div className="flex justify-center items-center mx-3 mt-3 overflow-hidden text-gray-700 bg-black shadow-lg bg-clip-border rounded-lg h-60 ">
+                    <div
+                        className="flex justify-center items-center mx-3 mt-3 overflow-hidden text-gray-700 bg-black shadow-lg bg-clip-border rounded-lg h-60 ">
                         <img src="photos/experience/Altura.png" alt="Altura-picture"
                              className="w-2/3 h-2/3 object-cover"/>
                     </div>
@@ -33,6 +34,20 @@ function Experiences() {
                             University of Windsor CSS
                         </h4>
                         <p className="text-gray-600">Full Stack Developer</p>
+                    </div>
+                </a>
+                <a href="https://revvitysignals.com"
+                   className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-lg w-80 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
+                    <div
+                        className="flex justify-center items-center mx-3 mt-3 overflow-hidden text-gray-700 bg-black shadow-lg bg-clip-border rounded-lg h-60 ">
+                        <img src="photos/experience/revvity.png" alt="Altura-picture"
+                             className="p-1 object-cover"/>
+                    </div>
+                    <div className="p-4 text-center">
+                        <h4 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                            Revvity Signals
+                        </h4>
+                        <p className="text-gray-600">Front End Software Developer</p>
                     </div>
                 </a>
             </div>
--- src/landing/Landing.jsx
+++ src/landing/Landing.jsx
@@ -1,68 +1,6 @@
 import React from 'react';
 
-// class Point {
-//     constructor(x, y) {
-//         this.x = x;
-//         this.y = y;
-//         this.lifetime = 0;
-//     }
-// }
-
 function Landing() {
-    // const canvasRef = useRef(null);
-    //
-    // useEffect(() => {
-    //     const canvas = canvasRef.current;
-    //     const ctx = canvas.getContext('2d');
-    //     const points = [];
-    //
-    //     const addPoint = (x, y) => {
-    //         const rect = canvas.getBoundingClientRect();  // Get canvas position and size
-    //         points.push(new Point(x - rect.left, y - rect.top));  // Adjust mouse position relative to canvas
-    //     };
-    //
-    //     const handleMouseMove = ({clientX, clientY}) => {
-    //         addPoint(clientX, clientY);
-    //     };
-    //
-    //     document.addEventListener('mousemove', handleMouseMove);
-    //
-    //     const animatePoints = () => {
-    //         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    //         const duration = (0.7 * (1 * 1000)) / 60; // Duration of point life
-    //
-    //         points.forEach((point, index) => {
-    //             point.lifetime += 1;
-    //             if (point.lifetime > duration) {
-    //                 points.shift();
-    //             } else {
-    //                 const lifePercent = point.lifetime / duration;
-    //                 const spreadRate = 7 * (1 - lifePercent);
-    //
-    //                 ctx.lineJoin = 'round';
-    //                 ctx.lineWidth = spreadRate;
-    //
-    //                 ctx.strokeStyle = `#ea580c`;
-    //
-    //                 ctx.beginPath();
-    //                 if (index > 0) {
-    //                     ctx.moveTo(points[index - 1].x, points[index - 1].y);
-    //                 }
-    //                 ctx.lineTo(point.x, point.y);
-    //                 ctx.stroke();
-    //             }
-    //         });
-    //         requestAnimationFrame(animatePoints);
-    //     };
-    //
-    //     animatePoints();
-    //
-    //     return () => {
-    //         document.removeEventListener('mousemove', handleMouseMove);
-    //     };
-    // }, []);
-    // <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}
-    //        className="absolute top-0 left-0 left-0 pointer-events-none"/>
     return (
         <div className="flex flex-1 items-center justify-center flex-col pt-32 relative">
 
